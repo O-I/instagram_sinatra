0dfdd64ae44d4b9d7fd3d3ff4b53d52681dbc27c
Rahul Horé <hore.rahul@gmail.com>
2013-11-20 18:56:59 -0500

    Fixes Twitter API issues

--- main.rb
+++ main.rb
@@ -21,7 +21,7 @@ end
 Instagram.configure do |config|
   config.client_id     = ENV['CLIENT_ID']
   config.client_secret = ENV['CLIENT_SECRET']
-  config.access_token  = ENV['ACCESS_TOKEN']
+  config.access_token  = ENV['ACCESS_COIN']
 end
 
 get '/pics' do
